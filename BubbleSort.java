982006b8bc750ffbe83c09a299fd28862b8bf23c
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-04-18 12:49:55 +0000

    works!! Yippee!!!

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BubbleSort
 {
 
@@ -16,7 +18,24 @@ public class BubbleSort
 			//The next pass will use one less comparison
     
 		//End Loop for passes
+		for (passes = 0; passes < myNums.length - 1; passes++)
+		{
+			for (comparisons = 0; comparisons < myNums.length - 1 - passes; comparisons++)
+			{
+				if (myNums[comparisons] > myNums[comparisons + 1])
+				{
+					tempNum = myNums[comparisons];
+					myNums[comparisons] = myNums[comparisons + 1];
+					myNums[comparisons + 1] = tempNum;
+				}
+			}
+		}
 
+	
+	
+	  System.out.println(Arrays.toString(myNums));
+	
+	
 
 
   }
